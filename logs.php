5e575b7658e279cf0436765aa503567d209f2c78
sielavic <63396872+sielavic@users.noreply.github.com>
2022-12-06 11:06:39 +0300

    Update logs.php

--- logs.php
+++ logs.php
@@ -1,11 +1,25 @@
-   $childs = $board->childs;
-        foreach ($childs as $child ){
-            $work_child = $child->macrotask;
-            $logs_child = $work_child->getSortLogs();
-            if (!empty($logs_child)) {
-                foreach ($logs_child as $e) {
+ $columns = $board->childs;
+        foreach ($columns as $column ){
+            $work_column = $column->macrotask;
+            $logs_column = $work_column->getSortLogs();
+            $cards = $column->childs;
+            if (!empty($logs_column)) {
+                foreach ($logs_column as $e) {
                     $e->value = 'column';
                     $logs->push($e);
                 }
             }
+            foreach ($cards as $card ) {
+                $work_card = $card->macrotask;
+                $logs_card = $work_card->getSortLogs();
+                if (!empty($logs_card)) {
+                    foreach ($logs_card as $x) {
+                        $x->card_id = $card->id;
+                        $x->value = 'card';
+                        $logs->push($x);
+                    }
+                }
+            }
+
         }
+        $logs = $logs->sortByDesc('date');
